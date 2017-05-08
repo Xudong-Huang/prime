036bb337a365abc8485282430f85f7bc65ae6d31
Xudong Huang <huangxu008@hotmail.com>
2017-05-08 17:45:51 +0800

    :art: cache small prime numbers

--- src/lib.rs
+++ src/lib.rs
@@ -34,24 +34,24 @@ pub fn prime(max: usize) -> Generator<'static, (), usize> {
     // early return
     //=========================
 
-    if max <= 2 {
-        return generator::Gn::new(|| 2);
-    }
-
-    // if max <= 210 {
-    //     return generator::Gn::new_scoped(move |mut s| {
-    //         let vec = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-    //                    73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
-    //                    157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
-    //         for i in vec.iter() {
-    //             if *i > max {
-    //                 break;
-    //             }
-    //             s.yield_with(*i);
-    //         }
-    //         done!();
-    //     });
+    // if max <= 2 {
+    //     return generator::Gn::new(|| 2);
     // }
+
+    if max <= 210 {
+        return generator::Gn::new_scoped(move |mut s| {
+            let vec = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+                       73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
+                       157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
+            for i in vec.iter() {
+                if *i > max {
+                    break;
+                }
+                s.yield_with(*i);
+            }
+            done!();
+        });
+    }
 
     // alloc the vec in heap, ignore the step=2 items(odd numbers)
     let mut vec = vec![true; (max + 1) / 2];
