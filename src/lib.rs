b4f1e055c8251b71d6f69ef48bb91242023efb7f
Xudong Huang <huangxu008@hotmail.com>
2019-07-12 20:18:38 +0800

    update to compile

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,22 @@
 #![feature(test)]
-#![feature(conservative_impl_trait)]
 
+#[macro_use]
 extern crate may;
 #[macro_use]
 extern crate generator;
 
 use may::coroutine;
+
+// quick algorithm for usize sqrt
+// fn fast_sqrt(n: usize) -> usize {
+//     let mut delta = 3;
+//     let mut square = 1;
+//     while square <= n {
+//         square += delta;
+//         delta += 2;
+//     }
+//     (delta >> 1) - 1
+// }
 
 fn filter(vec: &[bool], step: usize) {
     // step form beginning, ignore the very first one which is a prime number
@@ -25,9 +36,11 @@ pub fn prime(max: usize) -> impl Iterator<Item = usize> + 'static {
     // early return
     if max <= 210 {
         return generator::Gn::new_scoped(move |mut s| {
-            let vec = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-                       73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
-                       157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
+            let vec = [
+                2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
+                83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
+                173, 179, 181, 191, 193, 197, 199,
+            ];
             for i in vec.iter().take_while(|v| **v <= max) {
                 s.yield_with(*i);
             }
@@ -41,11 +54,14 @@ pub fn prime(max: usize) -> impl Iterator<Item = usize> + 'static {
     vec[0] = false;
 
     let top = (max as f32).sqrt() as usize;
+    // let top = fast_sqrt(max);
 
     // skip step=2 which is already filtered
-    coroutine::scope(|s| for i in prime(top).skip(1) {
-        let v = &vec;
-        s.spawn(move || filter(v, i));
+    coroutine::scope(|s| {
+        for i in prime(top).skip(1) {
+            let v = &vec;
+            go!(s, move || filter(v, i));
+        }
     });
 
     generator::Gn::new_scoped(move |mut s| {
